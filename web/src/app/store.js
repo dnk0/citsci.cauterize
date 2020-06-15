d88a483a89337b77e7102c2830d8225a737ebbc2
tim <tk@debian.localdomain>
2020-06-15 12:28:25 +0200

    removed redux form

--- web/src/app/store.js
+++ web/src/app/store.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../reducers/counter';
 import visibilityReducer from '../reducers/visibilityFilter'
 import scenarioReducer from '../reducers/scenarios'
-import { reducer as formReducer } from 'redux-form'
 
 export default configureStore({
     reducer: {
         counter: counterReducer,
         visibility: visibilityReducer,
-        scenarios: scenarioReducer,
-        form: formReducer
+        scenarios: scenarioReducer
     },
 });
